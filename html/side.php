367ec800bfbecf1ca1e857fe73bdb5fba5cc9003
Louie Mattia <lmattia@nagios.com>
2026-05-28 07:31:30 -0700

    Prep for release 4.5.13

--- html/side.php
+++ html/side.php
@@ -1,7 +1,7 @@
 <?php
 include_once(dirname(__FILE__).'/includes/utils.inc.php');
 
-$this_version = '4.5.12';
+$this_version = '4.5.13';
 $link_target = 'main';
 $theme = isset($cfg['theme']) ? $cfg['theme'] : 'dark';
 if ($theme != 'dark' && $theme != 'light') {
